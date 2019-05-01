e2e3e773ca2574394de3a9fd42643fd4baab5360
Ediz Ucar <edizucar@hotmail.com>
2019-05-01 12:52:05 +0100

    Adding constructor to matrix class
    -> parameters are rows and columns.
    -> sets up 2d array with these dimensions filled with 0s

--- matrix.js
+++ matrix.js
@@ -1,1 +1,18 @@
-
+class Matrix{
+  constructor(row, col) {
+    
+  this.rows = row;
+	this.cols = col;
+	
+    this.Mat = [];
+    
+    for (var i = 0; i < this.rows; i++) {
+      this.Mat.push([]);
+      for (var j = 0; j < this.cols; j++) {
+        this.Mat[i].push(0.0);
+      }
+    }
+    
+  }
+  
+}
