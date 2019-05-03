9dc756482ac08ef3227687d1634d2446746f4213
Ediz Ucar <edizucar@hotmail.com>
2019-05-03 11:28:51 +0100

    Adding inputs for the constructor in neural-network.js
    Also assigning attributes to the arguments.

--- neural-network.js
+++ neural-network.js
@@ -1,4 +1,28 @@
 class NeuralNetwork {
-	
+	constructor(inputs, hiddenLayers, outputs) {
+		/* 	inputs is a number describing how many inputs the neural network has.
+		
+			hiddenLayers is an array where the length is how many hidden layers there are,
+			Each integer in the array represents how many nodes there are in that hidden layer.
+			
+			outputs is the number of outputs the neural network has.
+			
+			This is a fully connected neural network structure.
+		*/
+		this.inputCount = inputs;
+		
+		this.hiddenLayerCount = [];
+		for (int i=0;i<hiddenLayers.length;i++) {
+			this.hiddenLayerCount.push(hiddenLayers[i]);
+		}
+		
+		this.outputCount = outputs;
+		
+		
+		
+		
+		
+		
+	}	
 	
 }
